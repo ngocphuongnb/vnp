823cb8a36e557a846ceaf687e7bbd29a96e49e24
nguyenngocphuongnb <nguyenngocphuongnb@e9673cbc-dfb1-2666-a026-ee3e77da0295>
2013-02-26 09:18:34 +0000

    Update system core

--- trunk/admincp/controllers/ct_type/ct_type.class.php
+++ trunk/admincp/controllers/ct_type/ct_type.class.php
@@ -15,26 +15,231 @@ class ct_type
 	{
 		global $request, $template, $db, $session;		
 	}
+}
+
+class vnp_form
+{
+	public $formField	= array();
+	private $fieldOpts	= array();
 	
-	public static function formField( $fieldata = array() )
+	public function __construct()
 	{
-		$xtpl = new XTemplate( 'form_field.tpl', DOC_ROOT . MY_ADMDIR . 'controllers/ct_type/' );
-		$xtpl->assign( 'INSTALL_DIR', INSTALL_DIR );
-		$xtpl->assign( 'ADMIN_DIR', ADMIN_DIR );
-		$xtpl->assign( 'MY_DOMAIN', VNP_DOMAIN );
-		$xtpl->assign( 'MY_DIR', VNP_MYDIR );
+		$this->formField = array();
 	}
 	
-	public static function formGenerator( $fieldArray = array() )
+	/*	
+	$fieldData = array(
+							'type'		=> 'input_field_type',
+							'name'		=> 'input_field_name',
+							'value'		=> 'input_field_default_value',
+							'id'		=> 'input_field_id',
+							'label'		=> 'Input label',
+							'tooltip'	=> 'Help text data by tooltip',
+							'help'		=> 'Normal help text',
+							'even'		=> 'Javascript even: onclick, onblur, onchange, onkeydown, onfocus...',
+							'callback'	=> 'javascript callback function'
+							
+							'options'	=> array(
+													array(
+															'value' => 'Option value',
+															'text'	=> 'Option text'
+														),
+													array(
+															'value' => 'Option value',
+															'text'	=> 'Option text'
+														),
+													array(
+															'value' => 'Option value',
+															'text'	=> 'Option text'
+														)
+												)
+						);
+					
+	*/
+	
+	private function CreateField( $fieldData = array() )
+	{
+		$xtpl = new XTemplate( 'form_field.tpl', DOC_ROOT . MY_ADMDIR . 'controllers/ct_type/' );
+		
+		if( isset( $fieldData['even'] ) && !empty( $fieldData['even'] ) )
+		{
+			if( isset( $fieldData['callback'] ) && !empty( $fieldData['callback'] ) )
+			{
+				$fieldData['evenmethod'] = $fieldData['even'] . '="' . $fieldData['callback'] . ';"';
+			}
+		}
+		$xtpl->assign( 'FIELD', $fieldData );
+		if( isset( $fieldData['help'] ) && !empty( $fieldData['help'] ) )
+		{
+			$xtpl->parse( 'main.help' );
+		}
+		switch( $fieldData['type'])
+		{
+			case 'textbox':
+				{
+					$xtpl->parse( 'main.textbox' );
+				}
+				break;
+			case 'date':
+				{
+					$xtpl->parse( 'main.date' );
+				}
+				break;
+			case 'textarea':
+				{
+					$xtpl->parse( 'main.textarea' );
+				}
+				break;
+			case 'radio':
+				{
+					if( isset( $fieldData['options'] ) && !empty( $fieldData['options'] ) )
+					{
+						if( is_array( $fieldData['options'] ) )
+						{
+							foreach( $fieldData['options'] as $_opt )
+							{
+								if( $fieldData['value'] == $_opt['value'] )
+								{
+									$_opt['checked'] = ' checked="checked"';
+								}
+								else
+								{
+									$_opt['checked'] = '';
+								}
+								$xtpl->assign( 'OPTION', $_opt );
+								$xtpl->parse( 'main.radio.options' );
+							}
+						}
+					}
+					$xtpl->parse( 'main.radio' );
+				}
+				break;
+			case 'checkbox':
+				{
+					if( isset( $fieldData['options'] ) && !empty( $fieldData['options'] ) )
+					{
+						if( is_array( $fieldData['options'] ) )
+						{
+							foreach( $fieldData['options'] as $_opt )
+							{
+								if( $fieldData['value'] == $_opt['value'] )
+								{
+									$_opt['checked'] = ' checked="checked"';
+								}
+								else
+								{
+									$_opt['checked'] = '';
+								}
+								$xtpl->assign( 'OPTION', $_opt );
+								$xtpl->parse( 'main.checkbox.options' );
+							}
+						}
+					}
+					$xtpl->parse( 'main.checkbox' );
+				}
+				break;
+			case 'selectbox':
+				{
+					if( isset( $fieldData['options'] ) && !empty( $fieldData['options'] ) )
+					{
+						if( is_array( $fieldData['options'] ) )
+						{
+							$i = 1;
+							foreach( $fieldData['options'] as $_opt )
+							{
+								if( $fieldData['value'] == $_opt['value'] )
+								{
+									$_opt['selected'] = ' selected="selected"';
+								}
+								else
+								{
+									$_opt['selected'] = '';
+								}
+								$_opt['index'] = $i;
+								$xtpl->assign( 'OPTION', $_opt );
+								$xtpl->parse( 'main.selectbox.options' );
+								$xtpl->parse( 'main.selectbox.options2' );
+								$i++;
+							}
+						}
+					}
+					$xtpl->parse( 'main.selectbox' );
+				}
+				break;
+		}
+		
+		$xtpl->parse( 'main' );
+		return $xtpl->text( 'main' );
+	}
+	
+	/*
+	$formData = array(
+						'header' => 'Form header',
+						'action' => 'Form action onsubmit',
+						'method' => 'Form submit methd'
+					);
+	*/
+	
+	public function create( $formData = array(), $fieldData = array() )
 	{
 		$xtpl = new XTemplate( 'myform.tpl', DOC_ROOT . MY_ADMDIR . 'controllers/ct_type/' );
 		$xtpl->assign( 'INSTALL_DIR', INSTALL_DIR );
 		$xtpl->assign( 'ADMIN_DIR', ADMIN_DIR );
 		$xtpl->assign( 'MY_DOMAIN', VNP_DOMAIN );
 		$xtpl->assign( 'MY_DIR', VNP_MYDIR );
+		$xtpl->assign( 'FORM_DATA', $formData );
+		
+		foreach( $fieldData as $field )
+		{
+			$xtpl->assign( 'FIELD_DATA', $this->CreateField( $field ) );
+			$xtpl->parse( 'main.field' );
+		}
 		
 		$xtpl->parse( 'main' );
 		return $xtpl->text( 'main' );
+	}
+	
+	/*
+	type: number, text
+	$nameArray = array(
+						array(
+								'name' => 'Name of input field to get value',
+								'type' => 'Type of value'
+							),
+						array(
+								'input_field_name' => 'Name of input field to get value',
+								'input_value_type' => 'Type of value'
+							),
+						array(
+								'input_field_name' => 'Name of input field to get value',
+								'input_value_type' => 'Type of value'
+							),
+					);
+	*/
+	
+	public static function getFormData( $nameArray = array(), $insertDb = true )
+	{
+		global $request;
+		
+		$return = array();
+		
+		if( $insertDb )
+		{
+			foreach( $nameArray as $_name )
+			{
+				if( empty( $_name['1'] ) ) $_name['1'] = 'text';
+				$return[$_name[0]] = vnp_db::SQLValue( $request->get( $_name['0'], 'post' ), $_name['1'] );
+			}
+		}
+		else
+		{
+			foreach( $nameArray as $_name )
+			{
+				$return[$_name[0]] = $request->get( $_name[0], 'post' );
+			}
+		}
+		
+		return $return;
 	}
 }
 
--- trunk/admincp/controllers/member/member.class.php
+++ trunk/admincp/controllers/member/member.class.php
@@ -29,14 +29,20 @@ class member
 					
 				case 'editprofile':
 					{
-						if( $request->get( 'action', 'get', '' ) )
-						{
-							$template->content = $this->formEditProfile();
-						}
-						else
+						if( $request->get( 'submit', 'post', '' ) )
 						{
 							$this->saveProfile();
 						}
+						$template->content = $this->formEditProfile();
+					}
+					break;
+				case 'accsetting':
+					{
+						if( $request->get( 'submit', 'post', '' ) )
+						{
+							$this->saveAccSetting();
+						}
+						$template->content = $this->formEditAccSetting();
 					}
 					break;
 			}
@@ -59,10 +65,10 @@ class member
                         <li><a href="#"><i class="icon-envelope"></i>Notifications<span class="alert-noty">15</span></a></li>
                         <li><a href="#"><i class="icon-briefcase"></i>My Account</a></li>
                         <li><a href="#"><i class="icon-file"></i>View Profile</a></li>
-                        <li><a href="/vnp/admincp/index.php?ctl=member&action=editprofile"><i class="icon-pencil"></i>Edit Profile</a></li>
-                        <li><a href="#"><i class="icon-cog"></i>Account Settings</a></li>
+                        <li><a href="' . MY_ADMDIR  . 'index.php?ctl=member&action=editprofile"><i class="icon-pencil"></i>Edit Profile</a></li>
+                        <li><a href="' . MY_ADMDIR  . 'index.php?ctl=member&action=accsetting"><i class="icon-cog"></i>Account Settings</a></li>
                         <li class="divider"></li>
-                        <li><a href="/vnp/admincp/index.php?ctl=member&action=logout"><i class="icon-off"></i><strong>Logout</strong></a></li>
+                        <li><a href="' . MY_ADMDIR  . 'index.php?ctl=member&action=logout"><i class="icon-off"></i><strong>Logout</strong></a></li>
           			</ul>';
 					
 		$customMenu['topmenu'][] = $html;
@@ -70,6 +76,66 @@ class member
 	
 	private function saveProfile()
 	{
+		global $db, $adminData;
+		
+		$profile = array(
+							array( 'realname' ),
+							array( 'gender', 'number' ),
+							array( 'telephone' ),
+							array( 'constellation' ),
+							array( 'birthday', 'date' ),
+							array( 'address' ),
+							array( 'company' )
+						);
+		$dbData = vnp_form::getFormData( $profile );
+
+		$dbData['birthday'] = explode( '-', str_replace( "'", "", $dbData['birthday'] ) );
+		$dbData['birthyear'] = $dbData['birthday'][0];
+		$dbData['birthmonth'] = $dbData['birthday'][2];
+		$dbData['birthday'] = $dbData['birthday'][1];
+		
+		$_result = $db->UpdateRows( USER_PROFILE, $dbData, array( 'userid' =>  $adminData['userid'] ) );
+		$_result = $db->UpdateRows( VNP_USER, array( 'realname' => $dbData['realname'] ), array( 'userid' =>  $adminData['userid'] ) );
+		if( !$_result )
+		{
+			$db->Kill();
+		}
+		else
+		{
+			Header( 'Location: ' . MY_ADMDIR . 'index.php?ctl=member&action=editprofile' );
+		}
+	}
+	
+	private function saveAccSetting()
+	{
+		global $db, $adminData;
+		
+		$profile = array(
+							array( 'email' ),
+							array( 'password' ),
+							array( 'repassword' )
+						);
+		$dbData = vnp_form::getFormData( $profile, false );
+		
+		$pass = new vnp_pass();
+		
+		if( $dbData['password'] == $dbData['repassword'] )
+		{
+			$dbData['password'] = vnp_db::SQLValue( $pass->genPass( $dbData['password'] ) );
+			$dbData['salt']		= vnp_db::SQLValue( $pass->salt );
+			unset( $dbData['repassword'] );
+		}
+		$dbData['email'] = vnp_db::SQLValue( $dbData['email'] );
+		
+		$_result = $db->UpdateRows( VNP_USER, $dbData, array( 'userid' =>  $adminData['userid'] ) );
+		if( !$_result )
+		{
+			$db->Kill();
+		}
+		else
+		{
+			Header( 'Location: ' . MY_ADMDIR . 'index.php?ctl=member&action=accsetting' );
+		}
 	}
 	
 	public static function adminLoginForm()
@@ -98,16 +164,205 @@ class member
 		echo $xtpl->out( 'main' );
 	}
 	
-	private function formEditProfile()
-	{	
-		$xtpl = new XTemplate( 'editprofile.tpl', DOC_ROOT . MY_ADMDIR . 'controllers/member/' );
-		$xtpl->assign( 'INSTALL_DIR', INSTALL_DIR );
-		$xtpl->assign( 'ADMIN_DIR', ADMIN_DIR );
-		$xtpl->assign( 'MY_DOMAIN', VNP_DOMAIN );
-		$xtpl->assign( 'MY_DIR', VNP_MYDIR );
+	private function formEditAccSetting()
+	{
+		global $db, $adminData;
 		
-		$xtpl->parse( 'main' );
-		return $xtpl->text( 'main' );
+		$checkUser = array( 'userid' => vnp_db::SQLValue( $adminData['userid'], 'number' ) );
+		
+		$db->SelectRows( VNP_USER, $checkUser );
+		if( $db->RowCount() === 1 )
+		{
+			$UserData = $db->RowArray(0, MYSQL_ASSOC);
+			
+			$form = new vnp_form();
+			
+			$fieldData = array();
+			
+			$fieldData[] = array( 
+									'type'		=> 'textbox',
+									'name'		=> 'email',
+									'label'		=> 'Email',
+									'tooltip'	=> 'Email',
+									'value'		=> $UserData['email']
+								);
+			$fieldData[] = array( 
+									'type'		=> 'textbox',
+									'name'		=> 'password',
+									'label'		=> 'Password',
+									'tooltip'	=> 'Nhập mật khẩu',
+									'value'		=> ''
+								);
+			$fieldData[] = array( 
+									'type'		=> 'textbox',
+									'name'		=> 'repassword',
+									'label'		=> 'RePassword',
+									'tooltip'	=> 'Nhập lại mật khẩu',
+									'value'		=> ''
+								);
+			$formData = array(
+								'header' => 'Cài đặt tài khoản',
+								'action' => MY_ADMDIR . 'index.php?ctl=member&action=accsetting',
+								'method' => 'post'
+							);
+							
+			return $form->create( $formData, $fieldData );
+		}
+		else return 'Cannot find user!';
+	}
+	
+	private function formEditProfile()
+	{
+		global $db, $adminData;
+		
+		$checkUser = array( 'userid' => vnp_db::SQLValue( $adminData['userid'], 'number' ) );
+		
+		$db->SelectRows( USER_PROFILE, $checkUser );
+		//echo $db->GetHTML();
+		if( $db->RowCount() === 1 )
+		{
+			$UserData = $db->RowArray(0, MYSQL_ASSOC);
+			
+			$UserData['birthday'] = str_pad((int) $UserData['birthday'], 2, '0',STR_PAD_LEFT );
+			$UserData['birthmonth'] = str_pad((int) $UserData['birthmonth'], 2, '0',STR_PAD_LEFT );
+		
+			$form = new vnp_form();
+			
+			$fieldData = array();
+			
+			$fieldData[] = array( 
+									'type'		=> 'textbox',
+									'name'		=> 'realname',
+									'label'		=> 'Tên đầy đủ',
+									'tooltip'	=> 'Tên đầy đủ',
+									'value'		=> $UserData['realname']
+								);
+								
+			$fieldData[] = array( 
+									'type'		=> 'radio',
+									'name'		=> 'gender',
+									'label'		=> 'Giới tính',
+									'tooltip'	=> 'Tên đầy đủ',
+									'value'		=> $UserData['gender'],
+									'options'	=> array(
+														array(
+																'value' => '0',
+																'text'	=> 'Không xác định'
+															),
+														array(
+																'value' => '1',
+																'text'	=> 'Nam'
+															),
+														array(
+																'value' => '2',
+																'text'	=> 'Nữ'
+															)
+													)
+								);
+			
+			$fieldData[] = array( 
+									'type'		=> 'selectbox',
+									'name'		=> 'constellation',
+									'label'		=> 'Chòm sao',
+									'tooltip'	=> 'Chòm sao cung hoàng đạo',
+									'value'		=> $UserData['constellation'],
+									'options'	=> array(
+														array(
+																'value' => '',
+																'text'	=> 'Chọn chòm sao'
+															),
+														array(
+																'value' => 'Bạch Dương',
+																'text'	=> 'Bạch Dương'
+															),
+														array(
+																'value' => 'Kim Ngưu',
+																'text'	=> 'Kim Ngưu'
+															),
+														array(
+																'value' => 'Song Tử',
+																'text'	=> 'Nữ'
+															),
+														array(
+																'value' => 'Cự Giải',
+																'text'	=> 'Cự Giải'
+															),
+														array(
+																'value' => 'Sư Tử',
+																'text'	=> 'Sư Tử'
+															),
+														array(
+																'value' => 'Xử Nữ',
+																'text'	=> 'Xử Nữ'
+															),
+															array(
+																'value' => 'Thiên Bình',
+																'text'	=> 'Thiên Bình'
+															),
+														array(
+																'value' => 'Thiên Yết',
+																'text'	=> 'Thiên Yết'
+															),
+														array(
+																'value' => 'Nhân Mã',
+																'text'	=> 'Nhân Mã'
+															),
+														array(
+																'value' => 'Ma Kết',
+																'text'	=> 'Ma Kết'
+															),
+														array(
+																'value' => 'Thủy Bình',
+																'text'	=> 'Thủy Bình'
+															),
+														array(
+																'value' => 'Song Ngư',
+																'text'	=> 'Song Ngư'
+															),
+													)
+								);
+								
+			$fieldData[] = array( 
+									'type'		=> 'date',
+									'name'		=> 'birthday',
+									'label'		=> 'Ngày sinh',
+									'tooltip'	=> 'Ngày sinh',
+									'value'		=> $UserData['birthday'] . '/' . $UserData['birthmonth'] . '/' . $UserData['birthyear']
+								);
+			
+			$fieldData[] = array( 
+									'type'		=> 'textbox',
+									'name'		=> 'telephone',
+									'label'		=> 'Điện thoại',
+									'tooltip'	=> 'Điện thoại',
+									'value'		=> $UserData['telephone']
+								);
+			
+			$fieldData[] = array( 
+									'type'		=> 'textarea',
+									'name'		=> 'address',
+									'label'		=> 'Địa chỉ',
+									'tooltip'	=> 'Địa chỉ',
+									'value'		=> $UserData['address']
+								);
+			
+			$fieldData[] = array( 
+									'type'		=> 'textbox',
+									'name'		=> 'company',
+									'label'		=> 'Công ty',
+									'tooltip'	=> 'Công ty',
+									'value'		=> $UserData['company']
+								);
+			
+			$formData = array(
+								'header' => 'Sửa thông tin cá nhân',
+								'action' => MY_ADMDIR . 'index.php?ctl=member&action=editprofile',
+								'method' => 'post'
+							);
+
+			return $form->create( $formData, $fieldData );
+		}
+		else return 'Cannot find the user!';
 	}
 }
 
